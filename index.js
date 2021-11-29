20afcb19e9aeca15e1ba8d05371612d2498a0be3
Marsel <you@example.com>
2021-11-29 23:36:27 +0100

    commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-{/* <li class="todo">
+/* <li class="todo">
   <div class="completed-section">
     <input class="completed-checkbox" type="checkbox" />
   </div>
@@ -15,96 +15,184 @@
   The completed one is exactly the same,
   but with an extra completed class in the li
   <li class="todo completed">
---> */}
+--> */
+
+// How to work with state:
+// 1. create some state
+// 2. render the app based on the state
+// 3. update the state
+// 4. rerender the app based on the new state
 
 
-let todos = [
-    {
-        text: 'Shopping',
-        completed: false
-    },
-    {
-        text: 'Work',
-        completed: true
+const showCompletedCheckbox = document.querySelector('.show-completed-checkbox')
+const addTodoForm = document.querySelector('.add-item')
+const todoList = document.querySelector('.todo-list')
+const completedList = document.querySelector('.completed-list')
+
+
+
+const state = {
+
+    todos: [
+        {
+            text: 'Shopping',
+            completed: false
+        },
+        {
+            text: 'Work',
+            completed: false
+        },
+        {
+            text: 'Home',
+            completed: true
+        }
+    ],
+    showCompleted: true
+
+}
+
+// Functions to get derived state
+function getCompletedTodos() {
+    return state.todos.filter(function (todo) {
+        return todo.completed
+    })
+}
+
+function getIncompleteTodos() {
+    return state.todos.filter(function (todo) {
+        return !todo.completed
+    })
+}
+
+function toggleTodo(todo) {
+    todo.completed = !todo.completed
+}
+
+function addTodo(todo) {
+    state.todos.push(todo)
+}
+
+function deleteTodo(text) {
+    state.todos = state.todos.filter(function (todo) {
+        return todo.text !== text
+    })
+}
+
+function editTodo(todo, newtext) {
+    todo.text = newtext
+}
+
+
+function renderTodo() {
+    const incompleteTodos = getIncompleteTodos()
+    todoList.innerHTML = ''
+
+    for (const todo of incompleteTodos) {
+        const todoLi = document.createElement('li')
+        todoLi.setAttribute('class', 'todo')
+
+        const todoDivSection = document.createElement('div')
+        todoDivSection.setAttribute('class', 'completed-section')
+        const inputElCheckbox = document.createElement('input')
+        inputElCheckbox.setAttribute('class', 'completed-checkbox')
+        inputElCheckbox.setAttribute('type', 'checkbox')
+        todoDivSection.append(inputElCheckbox)
+
+
+        const todoDivText = document.createElement('div')
+        todoDivText.setAttribute('class', 'text-section')
+        const pEl = document.createElement('p')
+        pEl.setAttribute('class', 'text')
+        pEl.textContent = todo.text
+        todoDivText.append(pEl)
+
+
+        const todoDivButton = document.createElement('div')
+        todoDivButton.setAttribute('class', 'button-section')
+        const buttonEdit = document.createElement('button')
+        buttonEdit.setAttribute('class', 'edit')
+        buttonEdit.textContent = "Edit"
+        const buttonDelette = document.createElement('button')
+        buttonDelette.setAttribute('class', 'delete')
+        buttonDelette.textContent = "Delete"
+        todoDivButton.append(buttonEdit, buttonDelette)
+
+        todoLi.append(todoDivSection, todoDivText, todoDivButton)
+
+
+
+        todoList.append(todoLi)
+
+        const completedCheckbox = todoLi.querySelector('.completed-checkbox')
+        completedCheckbox.checked = todo.completed
+        completedCheckbox.addEventListener('click', function () {
+            // toggle the todo
+            toggleTodo(todo)
+            // rerender the app
+            render()
+        })
+
+
     }
-]
-
-for (const todo of todos){
-    renderTodoCompleted(todo)
 }
 
 
-function renderTodo(todo){
-    const todoLi = document.createElement('li')
-    todoLi.setAttribute('class', 'todo')
+function renderTodoCompleted() {
 
-    const todoDivSection = document.createElement('div')
-    todoDivSection.setAttribute('class', 'completed-section')
-    const inputElCheckbox = document.createElement('input')
-    inputElCheckbox.setAttribute('class', 'completed-checkbox')
-    inputElCheckbox.setAttribute('type', 'checkbox')
-    todoDivSection.append(inputElCheckbox)
+    const completedTodos = getCompletedTodos()
+    completedList.innerHTML = ''
 
+    for (const todo of completedTodos) {
+        const todoLi = document.createElement('li')
+        todoLi.setAttribute('class', 'todo completed')
 
-    const todoDivText = document.createElement('div')
-    todoDivText.setAttribute('class', 'text-section')
-    const pEl = document.createElement('p')
-    pEl.setAttribute('class', 'text')
-    pEl.textContent = todo.text
-    todoDivText.append(pEl)
+        const todoDivSection = document.createElement('div')
+        todoDivSection.setAttribute('class', 'completed-section')
+        const inputElCheckbox = document.createElement('input')
+        inputElCheckbox.setAttribute('class', 'completed-checkbox')
+        inputElCheckbox.setAttribute('type', 'checkbox')
+        todoDivSection.append(inputElCheckbox)
 
 
-    const todoDivButton = document.createElement('div')
-    todoDivButton.setAttribute('class', 'button-section')
-    const buttonEdit = document.createElement('button')
-    buttonEdit.setAttribute('class', 'edit')
-    buttonEdit.textContent = "Edit"
-    const buttonDelette = document.createElement('button')
-    buttonDelette.setAttribute('class', 'delete')
-    buttonDelette.textContent = "Delete"
-    todoDivButton.append(buttonEdit, buttonDelette)
+        const todoDivText = document.createElement('div')
+        todoDivText.setAttribute('class', 'text-section')
+        const pEl = document.createElement('p')
+        pEl.setAttribute('class', 'text')
+        pEl.textContent = todo.text
+        todoDivText.append(pEl)
 
-    todoLi.append(todoDivSection, todoDivText, todoDivButton)
 
-    const todoUlList = document.querySelector(".todo-list")
-    todoUlList.append(todoLi)
+        const todoDivButton = document.createElement('div')
+        todoDivButton.setAttribute('class', 'button-section')
+        const buttonEdit = document.createElement('button')
+        buttonEdit.setAttribute('class', 'edit')
+        buttonEdit.textContent = "Edit"
+        const buttonDelette = document.createElement('button')
+        buttonDelette.setAttribute('class', 'delete')
+        buttonDelette.textContent = "Delete"
+        todoDivButton.append(buttonEdit, buttonDelette)
+
+        todoLi.append(todoDivSection, todoDivText, todoDivButton)
+
+        completedList.append(todoLi)
+
+        const completedCheckbox = todoLi.querySelector('.completed-checkbox')
+        completedCheckbox.checked = todo.completed
+        completedCheckbox.addEventListener('click', function () {
+            // toggle the todo
+            toggleTodo(todo)
+            // rerender the app
+            render()
+        })
+
+    }
 }
 
 
-function renderTodoCompleted(todo){
-    const todoLi = document.createElement('li')
-    todoLi.setAttribute('class', 'todo completed')
-
-    const todoDivSection = document.createElement('div')
-    todoDivSection.setAttribute('class', 'completed-section')
-    const inputElCheckbox = document.createElement('input')
-    inputElCheckbox.setAttribute('class', 'completed-checkbox')
-    inputElCheckbox.setAttribute('type', 'checkbox')
-    todoDivSection.append(inputElCheckbox)
-
-
-    const todoDivText = document.createElement('div')
-    todoDivText.setAttribute('class', 'text-section')
-    const pEl = document.createElement('p')
-    pEl.setAttribute('class', 'text')
-    pEl.textContent = todo.text
-    todoDivText.append(pEl)
-
-
-    const todoDivButton = document.createElement('div')
-    todoDivButton.setAttribute('class', 'button-section')
-    const buttonEdit = document.createElement('button')
-    buttonEdit.setAttribute('class', 'edit')
-    buttonEdit.textContent = "Edit"
-    const buttonDelette = document.createElement('button')
-    buttonDelette.setAttribute('class', 'delete')
-    buttonDelette.textContent = "Delete"
-    todoDivButton.append(buttonEdit, buttonDelette)
-
-    todoLi.append(todoDivSection, todoDivText, todoDivButton)
-
-    const todoCompletedList = document.querySelector(".completed-list")
-    todoCompletedList.append(todoLi)
-
-    
+function render() {
+    console.log(state)
+    renderTodo()
+    renderTodoCompleted()
 }
+
+render()
